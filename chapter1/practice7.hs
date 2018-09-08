66152f12cfbac426a851da6a632a7ee02c8e3a4a
y_ahiru <yahiru1121@gmail.com>
2018-09-09 00:50:31 +0900

    模範解答を追加

--- chapter1/practice7.hs
+++ chapter1/practice7.hs
@@ -5,10 +5,20 @@ import Data.Char
 -- rot13 char | ord char + 13 > 122 = chr (ord char + 13 - 122 + 64)
 -- rot13 char = chr (ord char + 13)
 
-rot13 char
-    | 65 <= ord char && ord char <= 77
-    || 97 <= ord char && ord char <= 109 = chr (ord char + 13)
-    | otherwise = chr (ord char - 13)
+-- アルファベット以外を入力されると変な挙動をするのでダメ
+-- rot13 char
+--     | 65 <= ord char && ord char <= 77
+--     || 97 <= ord char && ord char <= 109 = chr (ord char + 13)
+--     | otherwise = chr (ord char - 13)
+
+-- 解答はこれ。
+-- わざわざordしなくてもいいのか。そりゃそうだ。
+rot13 ch
+    |  'A' <= ch && ch <= 'M'
+    || 'a' <= ch && ch <= 'm' = chr $ ord ch + 13
+    |  'N' <= ch && ch <= 'Z'
+    || 'n' <= ch && ch <= 'z' = chr $ ord ch - 13
+    | otherwise = ch
 
 toRot13 "" = ""
 toRot13 (x:xs) = [rot13 x] ++ toRot13 xs
@@ -17,3 +27,4 @@ main = do
     print $ toRot13 "ABC"
     print $ toRot13 "NOP"
     print $ toRot13 "xyz"
+    print $ toRot13 "Hello, World"
